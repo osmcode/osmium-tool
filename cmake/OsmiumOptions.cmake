63af4316d33c3a82e5d3a3fec79ec30c899c3e85
Jochen Topf <jochen@topf.org>
2014-12-17 17:58:17 +0100

    Add 'Dev' build type. Remove need for OsmiumOptions.cmake.

--- cmake/OsmiumOptions.cmake
+++
@@ -1,9 +0,0 @@
-# Setup common compiler options adviced for building Osmium headers
-
-if(NOT MSVC)
-    #always compile with optimization, even in debug mode
-    set(CMAKE_CXX_FLAGS_DEBUG -O3)
-    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O3 -g")
-endif()
-
-message(STATUS "Some compiler and linker options were changed by OsmiumOptions")
