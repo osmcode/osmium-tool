25513b81930d1d1efda79a254f6c86643b0a1ada
Jochen Topf <jochen@topf.org>
2015-08-22 22:18:55 +0200

    Add some magic to enable folding on travis output.

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -19,11 +19,18 @@ if [ "${CXX}" = "g++" ]; then
     CC=gcc-4.8
 fi
 
+echo "travis_fold:start:cmake\nRunning cmake..."
 cmake -LA \
     -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
     ${WORKAROUND} \
     ..
+echo "travis_fold:end:cmake"
 
+echo "travis_fold:start:make\nRunning make..."
 make VERBOSE=1
+echo "travis_fold:end:make"
+
+echo "travis_fold:start:ctest\nRunning ctest..."
 ctest --output-on-failure
+echo "travis_fold:end:ctest"
 
