3c91ced8f5a1e96af61d59fdf11dff55de9e582c
Rodrigo Hausen <hausen@compscinet.org>
2022-01-17 16:23:35 -0500

    clenup extract/io.hpp

--- src/extract/io.hpp
+++ src/extract/io.hpp
@@ -3,24 +3,18 @@
 #include "../option_clean.hpp"
 
 class ExtractFile : public osmium::io::File {
-    using Base = osmium::io::File;
-    using Base::Base;
-
 public:
     const OptionClean m_clean;
 
-    ExtractFile(const osmium::io::File& file, OptionClean m_clean) : Base(file), m_clean(m_clean) {}
+    ExtractFile(const osmium::io::File& file, OptionClean m_clean) : osmium::io::File(file), m_clean(m_clean) {}
 };
 
 class ExtractReader : public osmium::io::Reader {
-    using Base = osmium::io::Reader;
-    using Base::Base;
-
     OptionClean m_clean;
 
 public:
     template <typename... TArgs>
-    explicit ExtractReader(const ExtractFile& file, TArgs&&... args) : Base(file, std::forward<TArgs>(args)...), m_clean(file.m_clean) {}
+    explicit ExtractReader(const ExtractFile& file, TArgs&&... args) : osmium::io::Reader(file, std::forward<TArgs>(args)...), m_clean(file.m_clean) {}
 
     inline osmium::memory::Buffer read() {
         auto buffer = osmium::io::Reader::read();
