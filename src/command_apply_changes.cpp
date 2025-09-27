c14f8673bfe7656959147bee0cc7e4d7f9c20d25
Alexander Farber <farber72@outlook.de>
2025-09-27 13:09:36 +0200

    Fix the call

--- src/command_apply_changes.cpp
+++ src/command_apply_changes.cpp
@@ -279,7 +279,7 @@ bool CommandApplyChanges::run() {
         m_output_file.set("locations_on_ways");
     }
 
-    warn_locations_on_ways_lost(m_input_files, *this);
+    warn_locations_on_ways_lost({m_input_file}, *this);
 
     m_vout << "Opening output file...\n";
     osmium::io::Writer writer{m_output_file, m_output_overwrite, m_fsync};
